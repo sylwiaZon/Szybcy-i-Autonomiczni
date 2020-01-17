73377710317edd86efde04ba2f5da3c564d021a0
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2020-01-17 17:59:09 +0100

    Add files via upload

--- szybcy_i_autonomiczni/src/init_handler.erl
+++ szybcy_i_autonomiczni/src/init_handler.erl
@@ -3,7 +3,11 @@
 
 -export([init/2, handle/2, terminate/3, allowed_methods/2]).
 
-init(Req, _Opts) -> {cowboy_rest, Req, #{}}.
+init(Req0, State) ->
+    Req = cowboy_req:reply(200, #{
+        <<"content-type">> => <<"text/plain">>
+    }, <<"Hello World!">>, Req0),
+    {ok, Req, State}.
 
 allowed_methods(Req, State) ->
     Methods = [<<"POST">>],
@@ -11,7 +15,7 @@ allowed_methods(Req, State) ->
 
 handlePost(Req) ->
   	Body = <<"<h1>This is a response for POST</h1>">>,
-		{ok, Req_} = cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>}], Body, Req),
+		{ok, Req_} = cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>,<<"text/html">>,<<"text/plain">>}], Body, Req),
 		{ok, Req_, #{}}.
 
 handle(Req, _) ->
