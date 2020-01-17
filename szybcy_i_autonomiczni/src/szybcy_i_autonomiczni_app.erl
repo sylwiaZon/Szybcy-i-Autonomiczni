ca16c898a9ed9d97ec372b9e9d7b937c8e04b9c6
sylwiaZon <sylwia.zon@gmail.com>
2020-01-17 12:54:44 +0100

    Erlang serwer

---
+++ szybcy_i_autonomiczni/src/szybcy_i_autonomiczni_app.erl
@@ -0,0 +1,18 @@
+-module(szybcy_i_autonomiczni_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+        {'_', [{"/getRoute", post_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8085}],
+        #{env => #{dispatch => Dispatch}}
+    ),
+	szybcy_i_autonomiczni_sup:start_link().
+
+stop(_State) ->
+	ok = cowboy:stop_listener(my_http_listener).
